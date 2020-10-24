19ff7e9f37069a441b7177f33b6fc695895abd95
Shailendra Verma <shailendravermag@gmail.com>
2020-10-24 10:36:17 +0100

    added bg param

--- batch and sh/bg.bat
+++ batch and sh/bg.bat
@@ -1,1 +1,7 @@
-powershell (Get-WmiObject -Namespace root/WMI -Class WmiMonitorBrightnessMethods).WmiSetBrightness(1,1)
+set bg=%1
+
+if %bg%=="" (
+    set bg=1
+)
+
+powershell (Get-WmiObject -Namespace root/WMI -Class WmiMonitorBrightnessMethods).WmiSetBrightness(1,%bg%)
